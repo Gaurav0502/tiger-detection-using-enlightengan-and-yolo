88576c87d7c1f22a03b869ea51cf35744774f7f8
Gaurav Pendharkar <mitugaurav15@gmail.com>
2023-10-05 00:55:21 +0530

    conversion of coco and pascol voc to yolo darknet

--- scripts/dataset_conversion/structure_data.py
+++ scripts/dataset_conversion/structure_data.py
@@ -10,15 +10,15 @@ PATH = "../data/atrw_anno_detection_train(1)/ImageSets/Main"
 TRAINVAL_LABELS = "../data/labels/trainval/"
 
 # Get the images for training and validation sets.
-train_images_fp = open(os.path.join(path, "train.txt"), "r")
+train_images_fp = open(r"C:\Users\mitug\video-analytics-project\data\atrw_anno_detection_train(1)\ImageSets\Main\train.txt", "r")
 train_images_id = train_images_fp.read().split("\n")
 
-val_images_fp = open(os.path.join(path, "val.txt"), "r")
+val_images_fp = open(r"C:\Users\mitug\video-analytics-project\data\atrw_anno_detection_train(1)\ImageSets\Main\val.txt", "r")
 val_images_id = val_images_fp.read().split("\n")
 
 
-src_root = "../data/labels/trainval/"
-dst_root = "../final_data/labels"
+src_root = r"C:\Users\mitug\video-analytics-project\data\labels\trainval"
+dst_root = r"C:\Users\mitug\video-analytics-project\final_data\dataset\labels"
 
 # Copy train labels to appropriate locations.
 for i in train_images_id:
@@ -31,8 +31,8 @@ for i in val_images_id:
         shutil.copy(os.path.join(src_root, i + ".txt"), os.path.join(dst_root, "val", i + ".txt"))
 
 # Copy the test labels to appropriate locations.
-src_root = "../data/labels/test/"
-dst_root = "../final_data/labels/"
+src_root = r"C:\Users\mitug\video-analytics-project\data\labels\test"
+dst_root = r"C:\Users\mitug\video-analytics-project\final_data\dataset\labels"
 
 for i in os.listdir(src_root):
     if i != "":
